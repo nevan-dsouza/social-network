4461dfb03d31a4f5c9f76240d079a08ea3b56ec6
Nevan <nev.kd2832@gmail.com>
2022-03-28 20:42:58 -0400

    added code to route files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require("../../controllers/thoughtController");
+
+// .../api/thoughts
+router
+  .route("/")
+  .get(getAllThoughts)
+  .post(createThought);
+
+// .../api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// .../api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions")
+  .post(addReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
